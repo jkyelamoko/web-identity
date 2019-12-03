6c84b8868d52cd4eaad0c5d5686d4b00e63df7f0
Dubois Florian <fdubois@devoteam.com>
2019-12-03 15:48:07 +0100

    First commit

---
+++ src/main/java/com/devhome/WebIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.devhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebIdentityApplication.class, args);
+    }
+
+}
